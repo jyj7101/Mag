7f5d90aff6447ff90cb77ed9fa6ae0463e242638
jyj7101 <55736595+jyj7101@users.noreply.github.com>
2023-01-27 01:51:02 +0900

    주사위 만드는중...

---
+++ Assets/Script/Dice.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    public Texture[] DiceTextures;
+
+    private MeshRenderer meshRenderer;
+
+    public int diceNum;
+
+    public float colorN;
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.mainTexture = DiceTextures[diceNum];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(Prevention());
+        }
+    }
+    
+    void DownDiceNum()
+    {
+        diceNum--;
+        meshRenderer.material.mainTexture = DiceTextures[diceNum];
+    }
+
+    IEnumerator Prevention()
+    {
+        while (colorN < 1)
+        {
+            meshRenderer.material.color = new Color(1, colorN, colorN, 1);
+            colorN += Time.deltaTime;
+            yield return null;
+        }
+        DownDiceNum();
+        colorN = 0;
+        meshRenderer.material.color = new Color(1, 1, 1, 1);
+    }
+    
+}
