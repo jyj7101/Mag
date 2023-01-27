a65977bd040a19aa830671e174e34a4ea59356d8
jyj7101 <55736595+jyj7101@users.noreply.github.com>
2023-01-27 14:45:44 +0900

    렌더피쳐 템플릿

---
+++ Assets/ReversalFeature.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+// 클래스 명 : 쉐이더 + 피쳐
+public class ReversalFeature : ScriptableRendererFeature
+{
+
+    [System.Serializable]
+    public class ReversalSettings  // 내가 사용할 쉐이더 세팅
+    {
+        public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+        public Material passMaterial = null;
+    }
+
+    public ReversalSettings settings = new ReversalSettings(); // 세팅 클래스 생성
+
+    class ReversalPass : ScriptableRenderPass
+    {
+        public Material passMaterial;
+        string profilerTag;
+
+        int ssdColorId;
+        int ssdDepthId;
+
+        RenderTargetIdentifier colorTargetIdentifier;
+
+        RenderTargetIdentifier cameraColorTexture;
+
+        public ReversalPass(string profilerTag)
+        {
+            this.profilerTag = profilerTag;
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            ssdColorId = Shader.PropertyToID("_SSDColor");
+            ssdDepthId = Shader.PropertyToID("_SSDDepth");
+
+            var depthTextureDescriptor = cameraTextureDescriptor;
+            depthTextureDescriptor.colorFormat = RenderTextureFormat.Depth;
+
+            cmd.GetTemporaryRT(ssdDepthId, depthTextureDescriptor, FilterMode.Point);
+            cmd.GetTemporaryRT(ssdColorId, cameraTextureDescriptor.width, cameraTextureDescriptor.height, 0, FilterMode.Bilinear, RenderTextureFormat.ARGB32);
+
+            colorTargetIdentifier = new RenderTargetIdentifier(ssdColorId);
+
+            ConfigureTarget(colorTargetIdentifier);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            cameraColorTexture = renderingData.cameraData.renderer.cameraColorTarget;
+
+            CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
+            ///////////////////////////// 
+
+            RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+            opaqueDesc.depthBufferBits = 0;
+
+            cmd.Blit(cameraColorTexture, colorTargetIdentifier);
+
+            cmd.Blit(colorTargetIdentifier, cameraColorTexture, passMaterial);
+
+            context.ExecuteCommandBuffer(cmd);
+            /////////////////////////////
+            cmd.Clear();
+
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+
+        }
+    }
+
+    private ReversalPass scriptablePass;
+
+    public override void Create()
+    {
+        scriptablePass = new ReversalPass("Final");
+        scriptablePass.passMaterial = settings.passMaterial;
+        scriptablePass.renderPassEvent = settings.renderPassEvent;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(scriptablePass);
+    }
+}
