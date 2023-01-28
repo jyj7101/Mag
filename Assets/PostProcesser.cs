4eb1095b1bba8d7aed024aedb48674f5819e2691
jyj7101 <55736595+jyj7101@users.noreply.github.com>
2023-01-29 05:58:41 +0900

    chromatic amberration 대충 완성

---
+++ Assets/PostProcesser.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcesser : MonoBehaviour
+{
+    public Volume pope;
+    public ChromaticAberration chromatic;
+    
+    public float inChromaticTime;
+    public float chromaticRunTime;
+    public float outChromaticTime;
+
+    private Coroutine _coroutine;
+    private IEnumerator a; 
+    private void Start()
+    {
+        TryGetComponent<Volume>(out pope);
+        pope.profile.TryGet(out chromatic);
+     
+    }
+
+
+    private void Update()
+    {
+        // 이벤트로 받아올 것
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            _coroutine = StartCoroutine(IntroChromatic());
+            a = IntroChromatic();
+            StartCoroutine(a);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _coroutine = StartCoroutine(InChromatic());
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _coroutine = StartCoroutine(OutChromatic());
+        }
+        
+        if(Input.GetKeyDown(KeyCode.X))
+            StopCoroutine(_coroutine);
+        
+    }
+
+    IEnumerator IntroChromatic()
+    {
+        chromatic.active = true;
+        while (chromatic.intensity.value < 1)
+        {
+            chromatic.intensity.value += inChromaticTime * Time.deltaTime;
+            yield return null;
+        }
+        chromatic.intensity.value = 1;
+    }
+
+    IEnumerator InChromatic()
+    {
+        chromatic.active = true;
+        chromatic.intensity.value = 1;
+        yield return new WaitForSeconds(chromaticRunTime);
+    }
+            
+    IEnumerator OutChromatic()
+    {
+        chromatic.active = true;
+        chromatic.intensity.value = 1;
+        while (chromatic.intensity.value > 0)
+        {
+            chromatic.intensity.value -= outChromaticTime * Time.deltaTime;
+
+            yield return null;
+        }
+
+        chromatic.active = false;
+        chromatic.intensity.value = 0;
+    }
+    
+    
+    
+}
